7e8ca103e65ce9d3220fb1f9c5f7a98aa1f84c02
dongwz <49752101+tungwz@users.noreply.github.com>
2025-10-13 11:31:32 +0800

    Update define.h

--- include/define.h
+++ include/define.h
@@ -26,7 +26,7 @@
 ! 3. If defined, debug information is output.
 #define CoLMDEBUG
 ! 3.1 If defined, range of variables is checked.
-#undef RangeCheck
+#define RangeCheck
 ! 3.1 If defined, surface data in vector is mapped to gridded data for checking.
 #undef SrfdataDiag
 
